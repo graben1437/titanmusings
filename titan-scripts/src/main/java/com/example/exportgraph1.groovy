b2ec91fa856463e8b248a6f632ccce892b7b24e0
David Robinson <robinsda@us.ibm.com>
2014-12-16 14:47:06 -0500

    add simple export graph script

---
+++ titan-scripts/src/main/java/com/example/exportgraph1.groovy
@@ -0,0 +1,17 @@
+conf = new BaseConfiguration()
+conf.setProperty('storage.backend', 'cassandra')
+conf.setProperty('storage.hostname', '9.30.182.235')
+conf.setProperty('cache.db-cache', 'true')
+conf.setProperty('cache.db-cache-clean-wait', '20')
+conf.setProperty('cache.db-cache-time', '180000')
+conf.setProperty('cache.db-cache-size', '0.25')
+
+g=TitanFactory.open(conf)
+
+out = new FileOutputStream('graphout.dat')
+
+GraphSONWriter.outputGraph(g, out)
+
+g.commit()
+
+out.close()
